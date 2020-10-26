b124154d9984948a04266ada8de039de5692ea53
user42 <aballea.louis@gmail.com>
2020-10-26 08:37:24 +0100

    last

--- ft_exit.c
+++ ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 17:43:19 by user42            #+#    #+#             */
-/*   Updated: 2020/10/23 09:28:21 by user42           ###   ########.fr       */
+/*   Updated: 2020/10/26 08:37:08 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int		ft_exit_need_line(char *tmp_str, int i, t_lst *lst)
 	return (1);
 }
 
-void	need_some_line(t_lst *lst, int *i, char **tmp_str)
+void	need_some_line(int *i, char **tmp_str)
 {
 	*tmp_str = NULL;
 	*i = -1;
@@ -67,7 +67,7 @@ int		ft_exit(t_lst *lst, char *my_path, char **flags)
 	int		i;
 	char	*tmp_str;
 
-	need_some_line(lst, &i, &tmp_str);
+	need_some_line(&i, &tmp_str);
 	if (lst->next)
 		tmp_str = (char *)lst->next->maillon;
 	while (lst->next && tmp_str[++i])
